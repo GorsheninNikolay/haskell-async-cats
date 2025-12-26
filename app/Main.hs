b7a977f6c039c4c5475200f9bcd0ea96c66b30c6
Николай Горшенин <kolya.gorshenin.2002@gmail.com>
2025-12-26 18:42:18 +0300

    cats save to files in static/ directory

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,19 @@ module Main (main) where
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
+import System.Directory (createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive)
+import System.FilePath ((</>))
 
 import Cat.Api (fetchCatImage, mkCatService, newCatManager, postCatFileMultipart)
 import Cat.Factory (collectUniqueCats, defaultCollectOptions)
-import Cat.Zip (catsToZip)
+import Cat.Types (CatImage (catBytes))
+import Cat.Zip (catsToZip, defaultCatEntryName)
 
 main :: IO ()
 main = do
+    let outDir = "static" </> "cats"
+    resetDir outDir
+
     mgr <- newCatManager
     let svc = mkCatService "http://algisothal.ru:8889"
     cats <-
@@ -19,7 +25,22 @@ main = do
 
     putStrLn "All cats collected"
 
-    let zipBytes = LBS.toStrict (catsToZip cats)
+    putStrLn ("Saving cats to: " ++ outDir)
+    mapM_ (\(i, c) -> BS.writeFile (outDir </> defaultCatEntryName i) (catBytes c)) (zip [1 ..] cats)
+
+    let zipLazy = catsToZip cats
+    let zipPath = outDir </> "cats.zip"
+    LBS.writeFile zipPath zipLazy
+
+    let zipBytes = LBS.toStrict zipLazy
     putStrLn ("Uploading zip, size=" ++ show (BS.length zipBytes) ++ " bytes")
     postCatFileMultipart mgr svc "cats.zip" zipBytes
     putStrLn "Upload OK"
+
+resetDir :: FilePath -> IO ()
+resetDir dir = do
+    exists <- doesDirectoryExist dir
+    if exists
+        then removeDirectoryRecursive dir
+        else pure ()
+    createDirectoryIfMissing True dir
