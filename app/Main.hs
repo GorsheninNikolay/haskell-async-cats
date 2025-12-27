e8ff6a2ebe8f8d1692b94fd2210677b0eab0f8ce
Sergey Polukhin <sergeydpol@gmail.com>
2025-12-27 13:14:59 +0300

    small fixes

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,27 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import System.Directory (createDirectoryIfMissing, doesDirectoryExist, removeDirectoryRecursive)
 import System.FilePath ((</>))
+import Network.HTTP.Client (Manager)
 
 import Cat.Api (fetchCatImage, mkCatService, newCatManager, postCatFileMultipart)
 import Cat.Factory (collectUniqueCats, defaultCollectOptions)
 import Cat.Types (CatImage (catBytes))
 import Cat.Zip (catsToZip, defaultCatEntryName)
 
-import Control.Concurrent.MSem (MSem, new, with)
+import Control.Concurrent.MSem (MSem)
+import qualified Control.Concurrent.MSem as MSem
 import Control.Concurrent.Async (async)
 
-processCollage :: Int -> Manager -> CatService -> FilePath -> IO()
+processCollage :: Int -> Manager -> Cat.Api.CatService -> FilePath -> IO ()
 processCollage collageId mgr svc baseDir = do
-    let outDir = baseDir </> ("collage-" ++ showcollageId)
+    let outDir = baseDir </> ("collage-" ++ show collageId)
     resetDir outDir
 
     cats <- collectUniqueCats
                 defaultCollectOptions
                 12
                 (fetchCatImage mgr svc)
-    mapM_ (\(i, c) -> BS.writeFile (outDir </> defaultCatEntryName i) (catBytes c)) (zip [1 ..] cats)
+    
     putStrLn "All cats collected"
 
     putStrLn ("Saving cats to: " ++ outDir)
@@ -45,18 +47,18 @@ main = do
 
     mgr <- newCatManager
     let svc = mkCatService "http://algisothal.ru:8889"
-    sem <- Msem.new maxCollageWorkers
+    sem <- MSem.new maxCollageWorkers
+    
     let loop collageId = do
         MSem.with sem $ do
-            put StrLn $ "Starting worker #" ++ show collageId
-            a <- async $ do
-                _ <- processCollage collageId mgr svc baseDir
-                putStrLn $ "Worker #" ++ show collageId ++ "finished."
-            wait a
+            putStrLn $ "Starting worker #" ++ show collageId
+            _ <- async $ do
+                processCollage collageId mgr svc baseDir
+                putStrLn $ "Worker #" ++ show collageId ++ " finished."
+            pure () 
         loop (collageId + 1)
+    
     loop 1
-
-	
 
 resetDir :: FilePath -> IO ()
 resetDir dir = do
